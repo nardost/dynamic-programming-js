019a7e719ec6c6e11110ab033f2078f8e894fa4e
Nardos Tessema <nardos.tessema@gmail.com>
2023-12-17 13:19:47 -0600

    Test cases modified.

--- tests/fib.test.js
+++ tests/fib.test.js
@@ -3,10 +3,17 @@ const { fib, dynamicFib } = require( '../src/utils/fib');
 
 describe('Fibonacci numbers with dynamic programming', () => {
     each([
-        [6, 8],
-        [50, 12586269025]
+        [63, 6557470319842],
+        [50, 12586269025],
+        [78, 8944394323791464]
     ]).test('The %sth Fibonacci number is %s.', (a, b) => {
-        expect(dynamicFib(a)).toBe(b);
+        expect(dynamicFib(a)).toBeLessThanOrEqual(b);
+    }),
+    each([40, 50, 60, 70, 75, 78]).test('The %sth Fibonacci number is less than or equal to ' + Number.MAX_SAFE_INTEGER, (a) => {
+        expect(dynamicFib(a)).toBeLessThanOrEqual(Number.MAX_SAFE_INTEGER);
+    }),
+    each([79, 80, 90]).test('The %sth Fibonacci number is greater than ' + Number.MAX_SAFE_INTEGER, (a) => {
+        expect(dynamicFib(a)).toBeGreaterThan(Number.MAX_SAFE_INTEGER);
     })
 });
-;
+
