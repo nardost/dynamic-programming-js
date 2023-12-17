6766a8579a6f2ad8f1cd9f83dd8b35034932cd53
Nardos Tessema <nardos.tessema@gmail.com>
2023-12-17 14:06:04 -0600

    More test cases added to grid traveler.

--- tests/grid.test.js
+++ tests/grid.test.js
@@ -3,7 +3,9 @@ const { gridTraveler, dynamicGridTraveler } = require('../src/utils/grid');
 describe('Grid Traveler', () => {
     test.each([
         [2, 3, 3],
+        [4, 5, 35],
         [10, 10, 48620],
+        [13, 15, 9657700],
         [20, 20, 35345263800],
         [20, 30, 11541847896480]
     ])('For a %sx%s grid, there are %p possible routes.', (m, n, p) => {
