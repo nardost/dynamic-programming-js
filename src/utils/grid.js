eb409b6bcf0359721c1e849be2309bd52724bf24
Nardos Tessema <nardos.tessema@gmail.com>
2023-12-17 13:57:35 -0600

    Grid traveler with dynamic programming tested.

--- src/utils/grid.js
+++ src/utils/grid.js
@@ -4,4 +4,13 @@ const gridTraveler = (m, n) => {
     return gridTraveler(m-1, n) + gridTraveler(m, n-1);
 };
 
-module.exports = { gridTraveler };
+const dynamicGridTraveler = (m, n, memo={}) => {
+    const key = m + ',' + n;
+    if(key in memo) return memo[key];
+    if(m === 0 || n === 0) return 0;
+    if(m === 1 && n === 1) return 1;
+    memo[key] = dynamicGridTraveler(m-1, n, memo) + dynamicGridTraveler(m, n-1, memo);
+    return memo[key];
+};
+
+module.exports = { gridTraveler, dynamicGridTraveler };
--- tests/grid.test.js
+++ tests/grid.test.js
@@ -1,9 +1,12 @@
-const { gridTraveler } = require('../src/utils/grid');
+const { gridTraveler, dynamicGridTraveler } = require('../src/utils/grid');
 
 describe('Grid Traveler', () => {
     test.each([
-        [2, 3, 3]
+        [2, 3, 3],
+        [10, 10, 48620],
+        [20, 20, 35345263800],
+        [20, 30, 11541847896480]
     ])('For a %sx%s grid, there are %p possible routes.', (m, n, p) => {
-        expect(gridTraveler(m,n)).toBe(p)
+        expect(dynamicGridTraveler(m,n)).toBe(p)
     })
 });
